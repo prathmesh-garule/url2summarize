8e2f602d12ff2bc729cf30bb1a725fdd759ca2c7
prathmesh-garule <prathmeshgarule49@gmail.com>
2023-07-19 00:06:37 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,70 @@
+// import React from "react";
+
+// import { logo } from "../assets";
+
+// const Hero = () => {
+//   return (
+//     <header className='w-full flex justify-center items-center flex-col'>
+//       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+//         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
+//         <button
+//           type='button'
+//           onClick={() =>
+//             window.open("https://github.com/TidbitsJS/Summize", "_blank")
+//           }
+//           className='black_btn'
+//         >
+//           GitHub
+//         </button>
+//       </nav>
+
+//       <h1 className='head_text'>
+//         Summarize Articles with <br className='max-md:hidden' />
+//         <span className='orange_gradient '>OpenAI GPT-4</span>
+//       </h1>
+//       <h2 className='desc'>
+//         Simplify your reading with Summize, an open-source article summarizer
+//         that transforms lengthy articles into clear and concise summaries
+//       </h2>
+//     </header>
+//   );
+// };
+
+// export default Hero;
+
+
+
+import {logo } from '../assets';
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+  <header className="w-full flex justify-center items-center flex-col " >
+  <nav className='flex justify-between w-full mb-10'>
+    
+    <img src={logo} alt="some logo"  className='w-28 object-contain '></img>
+
+    <button type="button" onClick={() => window.open('https://goggle.com')} className='black_btn'>
+      github
+    </button>
+</nav>
+
+<h1 className='head_text'> Summurize Articles with <br className='max-md:hidden'></br> 
+<span className='orange_gradient'>
+  Open AI GPT -3
+</span>
+</h1>
+<h2 className='desc'>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus alias vel animi iure iusto officia consequuntur dolorem quia assumenda a.
+
+</h2>
+ 
+  </header>
+
+
+  )
+}
+
+export default Hero
